7c6f1a7f9506d4d53dc2c3a0db21a4d81bc03001
Juanmi <Juanmi26304@gmail.com>
2025-03-15 00:50:56 +0100

    threejs task

---
+++ src/CameraPosition.js
@@ -0,0 +1,10 @@
+export const CAMERA_POSITIONS = [
+  { x: 400, y: 300, z: -500, rotationX: 0.5, rotationY: 2.5, rotationZ: 0 },
+  { x: 0, y: 100, z: -700, rotationX: 0, rotationY: 3, rotationZ: 0 },
+  { x: -650, y: 200, z: -300, rotationX: 0, rotationY: 4, rotationZ: 0 },
+  { x: -600, y: 200, z: 400, rotationX: 0, rotationY: 5, rotationZ: 0 },
+  { x: -300, y: 400, z: 500, rotationX: -1, rotationY: 5.5, rotationZ: -1 },
+  { x: -400, y: 200, z: 800, rotationX: 0, rotationY: 5.5, rotationZ: 0 },
+  { x: 100, y: 200, z: 750, rotationX: 0, rotationY: 6, rotationZ: 0 },
+  { x: 700, y: 200, z: 350, rotationX: 0, rotationY: 7, rotationZ: 0 },
+];
